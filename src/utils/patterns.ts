a0e4cd383ea85ab85e6fbf7d5c2c845821cd1aa7
Mohsin Ansari <mohsinansari0705@gmail.com>
2026-02-25 19:13:31 +0530

    defined vibration pattern

---
+++ src/utils/patterns.ts
@@ -0,0 +1,9 @@
+export const ONE_SECOND_IN_MS = 1000;
+
+export const PATTERN = [
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS
+];
